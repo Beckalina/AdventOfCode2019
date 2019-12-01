58ab412e18c827350bf32dd815919ad108692231
Waterfield <rwaterfi@amazon.co.uk>
2019-12-01 18:15:54 +0000

    Added comments

--- Day1/Day1a.java
+++ Day1/Day1a.java
@@ -2,7 +2,12 @@ import java.util.ArrayList;
 import java.io.*;
 import java.util.Scanner;
 
+// Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module,
+// take its mass, divide by three, round down, and subtract 2. E.g. For a mass of 14, divide by 3 and round down to
+// get 4, then subtract 2 to get 2.
+
 public class Day1a {
+    // divide by three, round down, and subtract 2
     public static int calculateFuel(int mass) {
         double f = (Math.floor(mass/3)) - 2;
         return (int)f;
@@ -15,6 +20,7 @@ public class Day1a {
         File file = new File("Day1\\Day1_input.txt");
         Scanner sc = new Scanner(file);
 
+        // read input and add to list modules
         while (sc.hasNextInt()) {
             modules.add(sc.nextInt());
         }
@@ -25,7 +31,7 @@ public class Day1a {
             int fuel = calculateFuel(m);
             totalFuel += fuel;
         }
+        // answer
         System.out.println(totalFuel);
-
     }
 }
--- Day1/Day1b.java
+++ Day1/Day1b.java
@@ -2,7 +2,13 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+// Part b extends part a with recursion.
+// For each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as
+// the input mass and repeat the process, continuing until a fuel requirement is zero or negative. E.g. the total fuel
+// required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+
 public class Day1b {
+    // calls recursively while f is positive
     public static int calculateFuel(int mass, int total) {
         double f = (Math.floor(mass/3)) - 2;
         if (f <= 0) {
@@ -15,17 +21,19 @@ public class Day1b {
     }
 
     public static void main(String[] args) throws Exception {
-
+        // mass of modules
         ArrayList<Integer> modules = new ArrayList<>();
+        // total fuel requirement per module
         ArrayList<Integer> fuelRequirements = new ArrayList<>();
 
         File file = new File("Day1\\Day1_input.txt");
         Scanner sc = new Scanner(file);
-
+        // read input and add to list modules
         while (sc.hasNextInt()) {
             modules.add(sc.nextInt());
         }
 
+        // calculate fuel requirement per module (recursive), add result to fuelRequirements
         for (Integer m : modules) {
             int modTotal = 0;
             int fuel = calculateFuel(m, modTotal);
@@ -33,10 +41,12 @@ public class Day1b {
         }
         System.out.println(fuelRequirements);
 
+        // sum fuelRequirements
         int totalFuel = 0;
         for (Integer f : fuelRequirements) {
             totalFuel += f;
         }
+        // answer
         System.out.println(totalFuel);
     }
 }
